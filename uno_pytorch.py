d3a4162b179fa735d9869ea17838c441cb3972bf
xduan7 <xduan7@gmail.com>
2018-08-27 11:50:03 -0500

    Added image for network and minor changes and dataset.

--- uno_pytorch.py
+++ uno_pytorch.py
@@ -348,8 +348,8 @@ def main():
     # Data loaders for training/validation ####################################
     dataloader_kwargs = {
         'shuffle': 'True',
-        'num_workers': multiprocessing.cpu_count() if use_cuda else 0,
-        # 'num_workers': 0,
+        # 'num_workers': multiprocessing.cpu_count() if use_cuda else 0,
+        'num_workers': 0,
         'pin_memory': True if use_cuda else False
     }
 
